1250a3f8603892c4d17ec9ebd37fdd8c5c5392f3
Paul Friedrich <mail@paulfriedrich.me>
2024-03-03 16:35:32 +0100

    remove pulse/scream

--- src/VBANdotnet.CLI/Program.cs
+++ src/VBANdotnet.CLI/Program.cs
@@ -1,8 +1,4 @@
-﻿using System.Diagnostics;
-using System.Net;
-using System.Runtime.InteropServices;
-using System.Text;
-//using TerraFX.Interop.PulseAudio;
+﻿using System.Net;
 using VBANdotnet.CLI;
 using Xt;
 
@@ -68,147 +64,14 @@ public static class Program
 		using XtStream stream = device.OpenStream(in deviceParams, null);
 		using XtSafeBuffer safe = XtSafeBuffer.Register(stream);
 
-		// pulse
-/*		string name = "Fooapp";
-		string streamName = "fooStream";
-
-		byte[] bName = Encoding.ASCII.GetBytes(name);
-		byte[] bStreamName = Encoding.ASCII.GetBytes(streamName);
-
-		sbyte[] sbName = Array.ConvertAll(bName, Convert.ToSByte);
-		sbyte[] sbStreamName = Array.ConvertAll(bStreamName, Convert.ToSByte);
-
-		fixed(sbyte* psbName = sbName)
-		fixed(sbyte* psbStreamName = sbStreamName)
-		{
-			pa_threaded_mainloop* loop = PulseAudio.pa_threaded_mainloop_new();
-			pa_mainloop_api* loop_api = PulseAudio.pa_threaded_mainloop_get_api(loop);
-
-			pa_context* context = PulseAudio.pa_context_new(loop_api, psbName);
-			PulseAudio.pa_context_set_state_callback(context, &context_state_cb, loop);
-			PulseAudio.pa_threaded_mainloop_lock(loop);
-
-			PulseAudio.pa_threaded_mainloop_start(loop);
-			PulseAudio.pa_context_connect(context, null, pa_context_flags_t.PA_CONTEXT_NOAUTOSPAWN, null);
-
-			while(true) {
-				pa_context_state_t context_state = PulseAudio.pa_context_get_state(context);
-				Debug.Assert(PulseAudio.PA_CONTEXT_IS_GOOD(context_state) == 1);
-				if (context_state == pa_context_state_t.PA_CONTEXT_READY) break;
-				PulseAudio.pa_threaded_mainloop_wait(loop);
-			}
-
-			Console.WriteLine("initialized");
-
-			// Create a playback stream
-			pa_sample_spec sample_specifications;
-			sample_specifications.format = PulseAudio.PA_SAMPLE_S32NE;
-			sample_specifications.rate = 48000;
-			sample_specifications.channels = 2;
-
-			pa_channel_map map;
-			PulseAudio.pa_channel_map_init_stereo(&map);
-
-			pa_stream* stream = PulseAudio.pa_stream_new(context, psbStreamName, &sample_specifications, &map);
-			PulseAudio.pa_stream_set_state_callback(stream, &stream_state_cb, loop);
-			PulseAudio.pa_stream_set_write_callback(stream, &stream_write_cb, loop);
-
-			// recommended settings, i.e. server uses sensible values
-			pa_buffer_attr buffer_attr;
-			buffer_attr.maxlength = (uint.MaxValue) -1;
-			buffer_attr.tlength = (uint.MaxValue) -1;
-			buffer_attr.prebuf = (uint.MaxValue) -1;
-			buffer_attr.minreq = (uint.MaxValue) -1;
-
-			// Settings copied as per the chromium browser source
-			pa_stream_flags_t stream_flags = pa_stream_flags_t.PA_STREAM_START_CORKED |
-														pa_stream_flags_t.PA_STREAM_INTERPOLATE_TIMING |
-														pa_stream_flags_t.PA_STREAM_NOT_MONOTONIC |
-														pa_stream_flags_t.PA_STREAM_AUTO_TIMING_UPDATE |
-														pa_stream_flags_t.PA_STREAM_ADJUST_LATENCY;
-
-			Console.WriteLine("connecting playback");
-			// Connect stream to the default audio output sink
-			Debug.Assert(PulseAudio.pa_stream_connect_playback(stream, null, &buffer_attr, stream_flags, null,
-				null) == 0);
-
-			// Wait for the stream to be ready
-			while(true)
-			{
-				Console.WriteLine("getting stream state");
-				pa_stream_state_t stream_state = PulseAudio.pa_stream_get_state(stream);
-				Debug.Assert(PulseAudio.PA_STREAM_IS_GOOD(stream_state) == 1);
-				Console.WriteLine("stream state: " + stream_state);
-				if (stream_state == pa_stream_state_t.PA_STREAM_READY) break;
-				PulseAudio.pa_threaded_mainloop_wait(loop);
-			}
-
-			Console.WriteLine("stream ready");
-
-			PulseAudio.pa_threaded_mainloop_unlock(loop);
-
-			// Uncork the stream so it will start playing
-			PulseAudio.pa_stream_cork(stream, 0, &stream_success_cb, loop);
-		}
-*/
-		// PulseAudio.pa_simple_new()
-		// PulseAudio.pa_context_get_sink_info_list()
 
 		AudioClient audioClient = new(IPAddress.Any, 6980);
 		audioClient.Start();
 		stream.Start();
-
-/*		ScreamClient screamClient = new("0.0.0.0", 4010);
-		screamClient.SetupMulticast(true);
-		screamClient.Multicast = "239.255.77.77";
-		screamClient.Connect();
-*/
 
 		while(true)
 		{
 			Thread.Sleep(100);
 		}
 	}
-
-/*	[UnmanagedCallersOnly]
-	private static unsafe void context_state_cb(pa_context *context, void *loop) {
-		Console.WriteLine("context_state_cb");
-		PulseAudio.pa_threaded_mainloop_signal((pa_threaded_mainloop*)loop, 0);
-	}
-
-	[UnmanagedCallersOnly]
-	private static unsafe void stream_state_cb(pa_stream *s, void *loop) {
-		Console.WriteLine("stream_state_cb");
-		PulseAudio.pa_threaded_mainloop_signal((pa_threaded_mainloop*)loop, 0);
-	}
-
-	[UnmanagedCallersOnly]
-	static unsafe void stream_write_cb(pa_stream *stream, nuint requested_bytes, void *userdata) {
-		//Console.WriteLine("writing stream " + requested_bytes);
-		nuint bytes_remaining = requested_bytes;
-		while (bytes_remaining > 0) {
-			byte *buffer = null;
-			nuint bytes_to_fill = 48000;
-			nuint i;
-
-			if (bytes_to_fill > bytes_remaining) bytes_to_fill = bytes_remaining;
-
-			PulseAudio.pa_stream_begin_write(stream, (void**) &buffer, &bytes_to_fill);
-
-			for (i = 0; i < bytes_to_fill; i += 2) {
-				buffer[i] = (byte)((i%100) * 40 / 100 + 44);
-				buffer[i+1] = (byte)((i%100) * 40 / 100 + 44);
-			}
-
-			PulseAudio.pa_stream_write(stream, buffer, bytes_to_fill, null, 0, pa_seek_mode_t.PA_SEEK_RELATIVE);
-
-			bytes_remaining -= bytes_to_fill;
-		}
-	}
-
-	[UnmanagedCallersOnly]
-	static unsafe void stream_success_cb(pa_stream *stream, int success, void *userdata) {
-		return;
-	}
-*/
 }
